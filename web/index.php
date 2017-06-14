6ce46889761dd675d330bd2bc51392f52b1e9e97
moi <moi@moi.com>
2017-06-14 14:58:06 +0200

    gestion des erreurs de routage n°2

--- web/index.php
+++ web/index.php
@@ -5,8 +5,18 @@ $controllerName = $_GET["controller"];
 //Définition du dossier racine du projet (ici le projet web)
 define("ROOT_PATH",dirname(__DIR__));
 
-var_dump(ROOT_PATH);
+//Définition du chemin du contrôleur
+$controllerPath = ROOT_PATH.'/src/controllers/'.$controllerName.'.php';
+
+//Test de l'existence du contrôleur
+if(!file_exists($controllerPath)){
+    //Envoie vers le fichier erreur
+    $controllerPath = ROOT_PATH.'/src/controllers/erreur.php';
+}
 
 //Exécution du contrôleur
-require ROOT_PATH.'/src/controllers/'.$controllerName.'.php';
+require $controllerPath;
+
+
+
 ?>
