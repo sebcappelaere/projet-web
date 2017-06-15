bfcc87cbb9997ea36444774cdbc1829a6bd0e397
moi <moi@moi.com>
2017-06-15 11:50:37 +0200

    Traitement du formulaire d'authentification administrateur

--- src/controllers/login-admin.php
+++ src/controllers/login-admin.php
@@ -1,4 +1,42 @@
 <?php
+//Initialisation du tableau des erreurs
+$errors = [];
+
+//Récupération des données postées
+$login = filter_input(INPUT_POST,'login',FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING);
+$isSubmitted = filter_has_var(INPUT_POST,'submit');
+
+
+if($isSubmitted){
+    //Validation des données
+    if(empty($login)){
+        $errors[] = "Vous devez saisir un identifiant";
+    }
+    if(empty($password)){
+        $errors[] = "Vous devez saisir un mot de passe";
+    }
+    //Traitement des données
+    //s'il n'y a pas d'erreurs
+    if(count($errors)==0){
+        $ok = $login == "admin" && $password == "123";
+        if($ok){
+            //Redirection vers la page d'administration
+            header("location:index.php?controller=accueil-admin");
+        } else {
+            $errors[] = "Vos informations d'identification sont incorrectes";
+        }
+    }
+    
+}
+
 //Affichage du formulaire
-renderView('login-admin');
+renderView('login-admin',
+    [
+        'errors' => $errors,
+        'login' => $login,
+        'pageTitle' => "Login administration"
+    ]
+);
+
 ?>
--- src/views/login-admin.php
+++ src/views/login-admin.php
@@ -1,13 +1,24 @@
+<!-- Affichage des erreurs -->
+<?php if(count($errors)>0): ?>
+    <div class="alert alert-danger">
+        <ul>
+            <?php foreach($errors as $item): ?>
+                <li> <?=$item?> </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
+
 <form method="post">
     <div class="form-group">
         <label>Votre identifiant</label>
-        <input type="text" name="login" class="form-control">
+        <input type="text" name="login" class="form-control" value="<?=$login?>">
     </div>
     <div class="form-group">
         <label>Votre mot de passe</label>
         <input type="password" name="password" class="form-control">
     </div>
     <div class="form-group">
-        <button type="submit" name="submit">Valider</button>
+        <button type="submit" name="submit" class="btn btn-primary">Valider</button>
     </div>
 </form>
